f64363293c8ec8e33bea4e1e1f421b3e54ba8e10
Chase Derr <Chisnits@users.noreply.github.com>
2017-10-26 22:33:38 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import YourTeam from './components/YourTeam/YourTeam';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="app-container">
+        <YourTeam />
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/YourTeam/YourTeam.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+import './YourTeam.css';
+
+class YourTeam extends Component {
+    render() {
+        return (
+            <div className="your-team-container">
+                <h5>Your Team</h5>
+                    <div className="summoner-card-wrapper">
+                        <div className="summoner-wrapper">
+                            <div className="summoner-container">
+                                <div className="character-icon"></div>
+                                <div className="spell-container">
+                                    <div className="summoner-spell"></div>
+                                    <div className="summoner-spell"></div>
+                                </div>
+                            </div>
+                            <h6 className="summoner-name">Chisnits</h6>
+                        </div>
+                        <div className="summoner-wrapper">
+                            <div className="summoner-container">
+                                <div className="character-icon"></div>
+                                <div className="spell-container">
+                                    <div className="summoner-spell"></div>
+                                    <div className="summoner-spell"></div>
+                                </div>
+                            </div>
+                            <h6 className="summoner-name">Chisnits</h6>
+                        </div>
+                        <div className="summoner-wrapper">
+                            <div className="summoner-container">
+                                <div className="character-icon"></div>
+                                <div className="spell-container">
+                                    <div className="summoner-spell"></div>
+                                    <div className="summoner-spell"></div>
+                                </div>
+                            </div>
+                            <h6 className="summoner-name">Chisnits</h6>
+                        </div>
+                        <div className="summoner-wrapper">
+                            <div className="summoner-container">
+                                <div className="character-icon"></div>
+                                <div className="spell-container">
+                                    <div className="summoner-spell"></div>
+                                    <div className="summoner-spell"></div>
+                                </div>
+                            </div>
+                            <h6 className="summoner-name">Chisnits</h6>
+                        </div>
+                        <div className="summoner-wrapper">
+                            <div className="summoner-container">
+                                <div className="character-icon"></div>
+                                <div className="spell-container">
+                                    <div className="summoner-spell"></div>
+                                    <div className="summoner-spell"></div>
+                                </div>
+                            </div>
+                            <h6 className="summoner-name">Chisnits</h6>
+                        </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default YourTeam;
